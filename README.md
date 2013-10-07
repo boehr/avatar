cd3199995feae5e44c1bf7948b3285d91f3da7cd
Thilo Schmalfuß <thilo@hojoki.com>
2013-10-07 16:36:06 +0200

    first version.

---
+++ README.md
@@ -0,0 +1,12 @@
+Avatar
+=================
+
+Small utility providing user avatar pics in various sizes with good caching.
+
+Setup
+-------------------------------------------------
+
+* install node.js and npm
+* .bin/avatar
+
+
---
+++ server.js
@@ -0,0 +1,108 @@
+'use strict';
+
+var Canvas = require('canvas');
+var http = require('http');
+var url = require('url');
+
+var crc32 	= require("easy-crc32");
+var cp		  = require("colour-proximity");
+
+exports.start = function() {
+
+  var port = 8888;
+
+  http.createServer(handler).listen(port);
+  console.log("Avatar Server Running on", port);
+
+  function handler (req, res) {
+
+    var params = url.parse(req.url, true).query;
+    console.log(params);
+
+    params.t = (params.t || '?').toUpperCase();
+    params.h = params.h || params.t;
+    params.c = params.c || determineColor(params.h);
+    params.s = Number(params.s) || 150;
+
+    if (req.method === 'GET') {
+
+      var canvas = new Canvas(params.s, params.s);
+      var ctx = canvas.getContext('2d');
+
+      drawAvatar(ctx, params.t, params.c);
+
+//      var out = fs.createWriteStream(__dirname + '/dummy.png');
+      var stream = canvas.createPNGStream();
+
+      stream.on('data', function(chunk){
+//        out.write(chunk);
+        res.write(chunk);
+      });
+
+      stream.on('end', function(chunk) {
+//        console.log('saved png');
+        res.end(chunk);
+      });
+
+      // TODO: etag, maxAge
+
+      res.writeHead(200, {
+        'Content-Type': 'image/png'
+      });
+
+
+      return;
+
+    }
+
+    // didn't expected this
+    var debugData = req.method + '  ' + req.url;
+    console.log(debugData);
+    res.writeHead(200);
+    res.end(debugData);
+  }
+
+  function determineColor(string, avoid) {
+    var crc = crc32.calculate(string);
+   	var hex = crc.toString(16);
+
+   	if(avoid !== undefined){
+   		if(avoid.avoid !== undefined){
+   			if(avoid.proximity === undefined){
+   				avoid.proximity = '30';
+   			}
+   			for(var i=0;i<(hex.length-6);i++){
+   				var sub = "#" + hex.substring(i, 6+i);
+   				if(cp.proximity(avoid.avoid, sub)>avoid.proximity){
+   					return sub;
+   				}
+   			}
+   			//todo Error that a sufficiently far colour could not be calculated. To be honest, it should try something else, I don't know what though.
+   		}
+   	}
+
+   	return "#" + hex.substring(0, 6);
+  }
+
+  function drawAvatar(ctx, initials, bgColor) {
+
+    var height = ctx.canvas.height;
+    var width = ctx.canvas.width;
+
+    initials = initials || '?';
+    bgColor = bgColor || 'rgb(155, 255, 115)';
+
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.font = Math.round(height * 0.66) + 'pt Helvetica Neue';
+    ctx.fillStyle = 'rgba(0, 0, 0, .5)';
+    var textSize = ctx.measureText(initials);
+    ctx.fillText(initials, (width - textSize.width)/2, (height + textSize.actualBoundingBoxAscent)/2);
+
+    ctx.save();
+  }
+
+};
+
+
